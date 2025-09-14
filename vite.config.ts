83cc81a1e92599cd003b05c6021bf410bfe37928
xiawt99-wq <xiawt99@gmail.com>
2025-09-14 15:58:14 +0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// If deploying to GitHub Pages under /<repo>/ path, change base to '/<repo>/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
